6d6c7c2c7a2eb15da405902bb157eba3897fd0d6
aryaptf <putraarya384@gmail.com>
2023-12-11 11:44:37 +0700

    EAS PWEB NEW

--- resources/views/indexkategori.blade.php
+++ resources/views/indexkategori.blade.php
@@ -7,8 +7,9 @@
         <div class="col-8">
             <form action="/kategori/hasil" method="post" class="form-vertical">
                 {{ csrf_field() }}
+                <label for="kategori">Pilih Kategori</label>
                 <div class="form-group row">
-                    <label for="kategori" class="col-xl-2 col-form-label">Pilih Kategori</label>
+
                     <div class="col-sm-10">
                         <select class="form-control" id="kategori" name="kategori">
                             @foreach($kategori as $k)
